c9dc0706eeb97760ded75a8c5bad6e5203079082
Asavela <asamzilikazi@gmail.com>
2023-03-06 07:41:46 -0800

    0-memset.c and main.h

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *simple_print_buffer - prints buffer in hexa
+ *@buffer: the address of memory to print
+ *@size: the size of the memory to print
+ *
+ *Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (s);
+}
